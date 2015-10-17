a0dd93e0fbf902c294501adc0ec6fe54d7e45b16
Oleg Kreimer <kremboman2@yahoo.com>
2015-10-17 13:23:10 +0300

    Added config object and removed hard dependencies

--- onepageapp.js
+++ onepageapp.js
@@ -1,35 +1,39 @@
 var OnePageApp = (function(){
-  var $menu, $container, $tpl,
+  var config = {},
   startTpl = location.pathname.split('/').pop();
-  var init = function(menu, container, tpl){
-    $menu = menu;
-    $container = container;
-    $tpl = tpl;
+  var init = function(menu, container, tpl, tplBase, basePath){
+    config.menu = menu;
+    config.container = container;
+    config.tplBase = tplBase || '/';
+    config.tpl = tplBase + tpl;
+    config.basePath = basePath || '/';
     menu.addEventListener('click', function(e){
       if(e.target.tagName === 'A'){
         e.preventDefault();
-        var tplPath = e.target.href.replace('onepageapp', 'templates')+'.html';
+        var tplPath = OnePageApp.config().tplBase + e.target.href.split('/').pop()+'.html';
         history.pushState({tpl: tplPath}, '', e.target.href);
         OnePageApp.dom.updateElement(tplPath, container);
       }
     });
     window.onpopstate = function(e){
+      var config = OnePageApp.config();
       if(!!e.state){
-        OnePageApp.dom.updateElement(e.state.tpl, OnePageApp.container());
+        OnePageApp.dom.updateElement(e.state.tpl, config.container);
       } else {
-        OnePageApp.dom.updateElement(OnePageApp.tpl(), OnePageApp.container());
+        OnePageApp.dom.updateElement(config.tpl, config.container);
       }
     };
 
-    if(startTpl != 'onepageapp')
-      $tpl = '/templates/'+startTpl+'.html';
-    OnePageApp.dom.updateElement($tpl, $container);
+    if(startTpl !== '' && startTpl !== config.basePath.slice(1))
+      config.tpl = config.tplBase+startTpl+'.html';
+
+    OnePageApp.dom.updateElement(config.tpl, config.container);
   };
   return {
     init: init,
-    container: function(){return $container;},
-    menu: function(){return $menu;},
-    tpl: function(){return $tpl;}
+    config: function(){
+      return config;
+    }
   };
 })();
 // ajax module //
